4e6491c0051cdb146789dfe4060c66e166cd59d4
faisalalbarkah1414 <faisalalbarkah1414@gmail.com>
2022-09-27 14:41:05 +0700

    Initial Commit

---
+++ YouTubeClone/YouTubeCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouTubeCloneApp.swift
+//  YouTubeClone
+//
+//  Created by faisalalbarkah on 27/09/22.
+//
+
+import SwiftUI
+
+@main
+struct YouTubeCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
